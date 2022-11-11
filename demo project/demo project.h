6c5a8bda166f0eff247250e7d7f4293cb20088e9
SOURAV MUKHERJEE <SMMT863@MGTECHSOFT.IN>
2022-11-11 23:40:27 +0530

    new code

---
+++ demo project/demo project.h
@@ -0,0 +1,32 @@
+
+// demo project.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CdemoprojectApp:
+// See demo project.cpp for the implementation of this class
+//
+
+class CdemoprojectApp : public CWinApp
+{
+public:
+	CdemoprojectApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CdemoprojectApp theApp;
